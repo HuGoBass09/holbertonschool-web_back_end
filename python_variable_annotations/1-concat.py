58eec77e7ef49dab3657c459b7f238efc69249e8
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-07-31 20:20:19 +0400

    1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""A simple Python module with type annotations."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A function to concatenate two strings"""
+    return str1 + str2
