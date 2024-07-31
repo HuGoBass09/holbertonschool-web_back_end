41f58ad5460e4bb22ac38d113620f6bfe2ede61a
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-07-31 20:23:37 +0400

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import math
+
+"""A simple Python module with type annotations."""
+
+
+def floor(n: float) -> int:
+    """A function which returns floor of a number"""
+    return math.floor(n)
