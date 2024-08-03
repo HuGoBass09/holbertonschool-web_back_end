77c6d555c937a3f00ec06dd1d008f251a1acdec4
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-03 16:30:15 +0400

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,8 @@ from typing import List
 
 
 async def async_comprehension() -> List[float]:
-    """A function to return 10 random numbers"""
+    """
+    A function to return 10 random numbers
+    using async_comprehension
+    """
     return [i async for i in async_generator()]
