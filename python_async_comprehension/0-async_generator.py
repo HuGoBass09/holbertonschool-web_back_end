6b8730c062baecbfcf5eec4c055ac3e8daab7b8c
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-03 16:19:18 +0400

    0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 """A function that uses async comprehension"""
 
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Async Generator that yields random number
     """
