33eb4f59b7bbd3feccdb4d1130ecf16d4acb9554
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-04 22:38:51 +0400

    0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Caching implementation """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """A function to add an element to the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """A function to get an item using the given key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+
+        return None
