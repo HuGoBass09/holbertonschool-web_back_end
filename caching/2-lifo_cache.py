26a505f4ee53f980ec914480b9b5ac83ad5bed3e
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-04 23:44:30 +0400

    2

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Caching implementation """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """FIFOCache class"""
+
+    def put(self, key, item):
+        """A function to add an element to the cache"""
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                last_item = list(self.cache_data.keys())[len(self.cache_data) - 1]
+                print(f"DISCARD: {last_item}")
+                self.cache_data.pop(last_item)
+
+    def get(self, key):
+        """A function to get an item using the given key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
