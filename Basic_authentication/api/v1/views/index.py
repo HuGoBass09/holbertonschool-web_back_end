9f44746efdb993e0737fae9868580ea1fb93a473
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-18 03:06:27 +0400

    1

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
+@app_views.route("/unauthorized", methods=['GET'], strict_slashes=False)
 def unauthorized_abort() -> str:
     """GET /api/v1/stats
     Return:
