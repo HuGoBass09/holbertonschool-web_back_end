cfea864cd4b82347651a4c2eb35ca417345a521a
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-25 21:25:11 +0400

    14

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -68,3 +68,11 @@ class Auth:
             return user
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """A method which deletes a user from db"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except Exception:
+            return
