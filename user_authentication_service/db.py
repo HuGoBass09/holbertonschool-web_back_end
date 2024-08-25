5af04fab03f0f958ec7044c72540d8f84f0b37ed
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-08-25 20:37:13 +0400

    1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""DB module"""
+"""DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -14,7 +15,7 @@ class DB:
 
     def __init__(self) -> None:
         """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -28,17 +29,14 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database and return the User object"""
+        """Save the user to the database"""
         user = User(email=email, hashed_password=hashed_password)
-
         self._session.add(user)
-
         self._session.commit()
-
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """A method to find a spesific user"""
+        """find user method"""
         session = self._session
         try:
             results = session.query(User).filter_by(**kwargs).first()
@@ -47,3 +45,13 @@ class DB:
             return results
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """update data for user"""
+        session = self._session
+        updated_user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(updated_user, key):
+                raise ValueError
+            setattr(updated_user, key, value)
+        session.commit()
