318748dec5c1df9fad9a738a1c390fdbc07c7d78
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-12 10:48:29 +0400

    1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -23,3 +23,19 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
+        """A method to get key from redis"""
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_str(self, string: bytes) -> str:
+        """A method to get a string"""
+        return string.decode("utf-8")
+
+    def get_int(self, number: bytes) -> int:
+        """A method to get an integer value"""
+        result = int(number)
+        return result
