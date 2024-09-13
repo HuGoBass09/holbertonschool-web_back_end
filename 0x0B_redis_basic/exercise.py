481b59be95546ac8731876a90cf54bd962839227
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-13 10:34:24 +0400

    3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -23,6 +23,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """A decorator to store the history of inputs and
+    outputs for a particular function"""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper of decorator"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     """Cache redis class"""
 
@@ -32,6 +49,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: UnionOfTypes) -> str:
         """A method to store data into redis cache"""
         key = str(uuid4())
