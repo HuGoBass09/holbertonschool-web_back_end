96d8656598b2f41c55a349728c754f27e5b56b57
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-15 21:56:41 +0400

    4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -3,8 +3,6 @@
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE name = NEW.item_name;
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE NEW.item_name = name;
