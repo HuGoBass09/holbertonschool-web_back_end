4e4148defaa42a3473af06090b4f9a51f2566ee6
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-15 17:33:29 +0400

    0

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates table users
+-- Table creation script 
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255)
+);
