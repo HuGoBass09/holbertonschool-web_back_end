25ec8688ccae9352070ee9fae0df0c2033639473
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-22 23:01:24 +0400

    8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""MongoDB module"""
+
+import pymongo
+
+
+def list_all(mongo_collection) -> list:
+    """A function to list all documents in a collection"""
+    mongo_collection = mongo_collection.find()
+    if mongo_collection.count() == 0:
+        return []
+
+    return list(mongo_collection)
