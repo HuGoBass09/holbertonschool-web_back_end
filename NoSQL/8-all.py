cf569a9277b0106854114ed51373da40ad18ef2c
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-22 23:06:30 +0400

    8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -6,8 +6,8 @@ import pymongo
 
 def list_all(mongo_collection: pymongo) -> list:
     """A function to list all documents in a collection"""
-    mongo_collection = mongo_collection.find()
+    query = mongo_collection.find()
     if mongo_collection.count() == 0:
         return []
 
-    return list(mongo_collection)
+    return list(query)
