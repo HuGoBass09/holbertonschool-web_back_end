563c99c3d351f699458c2fc0a805a22c3c78e202
HuGoBass09 <ruslansadiqov76@gmail.com>
2024-09-29 23:08:36 +0400

    8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](dataType) {
+    if (dataType === 'string') {
+      return `${this._location}`;
+    }
+    if (dataType === 'number') {
+      return `${this._size}`;
+    }
+    return `${this._location}`; 
+  }
+}
